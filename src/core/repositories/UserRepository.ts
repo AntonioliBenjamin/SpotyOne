9d1a8310d0f8e61134499b324025007445a16c2d
Cédric Herouard <115704191+Bric66@users.noreply.github.com>
2022-11-21 09:32:03 +0100

    test

--- src/core/repositories/UserRepository.ts
+++ src/core/repositories/UserRepository.ts
@@ -7,21 +7,13 @@ import {RemoveAlbumInput} from "../Usecases/user/RemoveAlbumPropertiesToLibrary"
 import {RemoveTrackInput} from "../Usecases/user/RemoveTrackPropertiesToLibrary";
 
 export interface UserRepository {
-    create(input: User): Promise<User>;
+    create(userInput: User): Promise<User>;
 
     getByEmail(email: string): Promise<User>;
 
-    getById(userId: string): Promise<User>;
+    getById(id: string): Promise<User>;
 
-    update(input: UserUpdatedInput): Promise<User>;
+    update(userInput: UserUpdatedInput): Promise<User>;
 
-    delete(input: UserDeletedInput): Promise<User>;
-
-    addAlbum(input: AddAlbumInput): Promise<User>;
-
-    removeAlbum(input: RemoveAlbumInput): Promise<User>;
-
-    addTrack(input: AddTrackInput): Promise<User>;
-
-    removeTrack(input: RemoveTrackInput): Promise<User>;
+    delete(userInput: User): Promise<User>;
 }
